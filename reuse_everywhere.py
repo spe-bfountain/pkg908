b8e4da76f44f92bf52e6e1a4a7d3b51499f1d14f
Brett Fountain <bfountain@spe.org>
2019-08-06 10:27:30 -0500

    setting up a test for cookiecutter

---
+++ reuse_everywhere.py
@@ -0,0 +1,6 @@
+""" purpose of this popular python code: 
+analyze usno.mil NTP packets, infer electron drift from cesium atom, find nearest galaxy with same inverse drift, correlate electron drift with earth's EMF angles of primary dissonance, render the waveform for remote quark manipulation, and finally originate a text message to Mom from remote galaxy so as to have been delivered here n seconds in the past """
+
+
+print "...3 ,2 ,1 ...gnitset !dlrow ,olleh"
+
